d78e25beb3bc7aed7680da358c0fb613aaf35b71
khaled71612000 <khaled71612000@gmail.com>
2024-07-20 16:57:32 +0300

    Update GoonActor.h

--- Source/UnrealThreads/Goons/GoonActor.h
+++ Source/UnrealThreads/Goons/GoonActor.h
@@ -34,3 +34,93 @@ private:
 	const float DequeueInterval = 1.0f; // Time interval between dequeues in seconds
 	const int32 MaxDequeuesPerTick = 5; // Max number of items to dequeue per tick
 };
+
+//LogTemp: Warning: RunnableGoon Constructor called!
+//LogTemp : Warning : RunnableGoon Init called!
+//LogTemp : Warning : Thread started with priority : 0, Thread ID : 35280
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//PIE : Server logged in
+//PIE : Play in editor total start time 0.285 seconds.
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Prime number : 2, Thread ID : 32528
+//LogTemp : Warning : Prime number : 3, Thread ID : 32528
+//LogTemp : Warning : Prime number : 5, Thread ID : 32528
+//LogTemp : Warning : Prime number : 7, Thread ID : 32528
+//LogTemp : Warning : Prime number : 11, Thread ID : 32528
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Prime number : 13, Thread ID : 32528
+//LogTemp : Warning : Prime number : 17, Thread ID : 32528
+//LogTemp : Warning : Prime number : 19, Thread ID : 32528
+//LogTemp : Warning : Prime number : 23, Thread ID : 32528
+//LogTemp : Warning : Prime number : 29, Thread ID : 32528
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Prime number : 31, Thread ID : 32528
+//LogTemp : Warning : Prime number : 37, Thread ID : 32528
+//LogTemp : Warning : Prime number : 41, Thread ID : 32528
+//LogTemp : Warning : Prime number : 43, Thread ID : 32528
+//LogTemp : Warning : Prime number : 47, Thread ID : 32528
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Prime number : 53, Thread ID : 32528
+//LogTemp : Warning : Prime number : 59, Thread ID : 32528
+//LogTemp : Warning : Prime number : 61, Thread ID : 32528
+//LogTemp : Warning : Prime number : 67, Thread ID : 32528
+//LogTemp : Warning : Prime number : 71, Thread ID : 32528
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Prime number : 73, Thread ID : 32528
+//LogTemp : Warning : Prime number : 79, Thread ID : 32528
+//LogTemp : Warning : Prime number : 83, Thread ID : 32528
+//LogTemp : Warning : Prime number : 89, Thread ID : 32528
+//LogTemp : Warning : Prime number : 97, Thread ID : 32528
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogTemp : Warning : Exiting FindPrimes method with 1229 primes found!Thread ID : 35280
+//LogSlate : Updating window title bar state : overlay mode, drag disabled, window buttons hidden, title bar hidden
+//LogWorld : BeginTearingDown for / Game / UEDPIE_0_Untitled
+//LogTemp : Warning: Stop method called!Thread ID : 32528
+//LogTemp : Warning : Thread exiting Run method!Thread ID : 35280
+//LogTemp : Warning : Stop method called!Thread ID : 32528
+//LogTemp : Warning : RunnableGoon Destructor called!
